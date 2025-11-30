93f9c7600b5a39a22b4a0b62b8d320c7b1746f1f
Dorian-Cornu <dorian.cornu0@gmail.com>
2025-11-30 18:35:34 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,19 +1,19 @@
 from tkinter import Tk, Frame, Label, StringVar, Entry, Button
 from random import choice
 
-WORD_LIST = [
+Liste_Teste = [
     "AMOUR","MAJOR","ROUGE","BLEUE","TABLE","CHAIR","MONDE","VOYER","FABLE",
     "ARBRE","POMME","VACHE","FLEUR","NUAGE","LIVRE","RIREZ","TENIR","PLAGE",
     "GAMME","LUMEN","BRUIT","SONAR","COEUR","OCEAN","SANTE","POIDS","ETAGE",
 ]
 
-WORD_LENGTH = 5
-MAX_TRIES = 6
+Longeur = 5
+Essai_max = 6
 
 class Motus:
     def __init__(self, ecran):
         ecran.title("Motus")
-        self.__mot = choice(WORD_LIST).upper()
+        self.__mot = choice(Liste_Teste).upper()
         print(self.__mot)
         self.__essai = 0
         self.__game_over = False
@@ -22,9 +22,9 @@ class Motus:
         self.__frame_entree = Frame(ecran, pady=5)
         self.__frame_entree.grid(row=1, column=0)
         self.__grille = [] 
-        for ligne in range(MAX_TRIES):
+        for ligne in range(Essai_max):
             l = []
-            for colonne in range(WORD_LENGTH):
+            for colonne in range(Longeur):
                 case = Label(self.__frame_grid, text=" ", width=4, height=2, relief="solid", font=("Helvetica", 18, "bold"))
                 case.grid(row=ligne, column=colonne, padx=3, pady=3)
                 l.append(case)
@@ -34,7 +34,7 @@ class Motus:
         self.__valider.grid(row=0, column=0, padx=5)
         self.__bouton = Button(self.__frame_entree, text="Essayer", command=self.essai_mot)
         self.__bouton.grid(row=0, column=1, padx=5)
-        self.__texte = Label(ecran, text=f"Mot à deviner : {WORD_LENGTH} lettres — {MAX_TRIES} essais", pady=5)
+        self.__texte = Label(ecran, text=f"Mot à deviner : {Longeur} lettres — {Essai_max} essais", pady=5)
         self.__texte.grid(row=2, column=0)
 
     def essai_mot(self):
@@ -49,20 +49,20 @@ class Motus:
             case.config(text=ch)
             state = resultat[i]
             if state == 'juste':
-                case.config(bg="#6aaa64")  
+                case.config(bg="green")  
             elif state == 'present':
-                case.config(bg="#c9b458")  
+                case.config(bg="yellow")  
             else:
-                case.config(bg="#787c7e")  
+                case.config(bg="grey")  
         self.__essai += 1
         if teste == self.__mot:
             self.__game_over = True
             self.__texte.config(text=f"Victoire")
-        elif self.__essai >= MAX_TRIES:
+        elif self.__essai >= Essai_max:
             self.__game_over = True
             self.__texte.config(text=f"Perdu")
         else:
-            self.__texte.config(text=f"Essai {self.__essai}/{MAX_TRIES}")
+            self.__texte.config(text=f"Essai {self.__essai}/{Essai_max}")
 
     def evaluer_mot(self, mot_essai, mot):
         resultat = ['absent'] * len(mot_essai)
